66d9c1ec2c78ad2dace9edac1fb7cc2e45178545
Vatsalya Goel <vatsalyagoel@gmail.com>
2016-10-19 04:26:54 +1100

    Adding Week 7

---
+++ Week 7/25404776-socket-client.c
@@ -0,0 +1,75 @@
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <stdio.h>  
+#include <string.h>  
+#include <stdlib.h> 
+#include <arpa/inet.h> 
+#include <unistd.h>
+
+
+int main() 
+{  
+
+    SEG_DATA *mydata;
+
+    int sock_desc = socket(AF_INET, SOCK_STREAM, 0); /*Setting up socket for connection*/
+    if (sock_desc == -1)
+    {
+        printf("cannot create socket!\n");
+        return -1;
+    }
+
+    struct sockaddr_in client;  /*Connecting to localhost port*/
+    memset(&client, 0, sizeof(client));  
+    client.sin_family = AF_INET;  
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");  
+    client.sin_port = htons(8080);  
+
+    if (connect(sock_desc, (struct sockaddr*)&client, sizeof(client)) != 0) /*Error handling*/
+    {
+        printf("cannot connect to server!\n");
+        close(sock_desc);
+        return -1;
+    }
+
+    char buf[100]; /*Setting data limit to 100 caharacters*/
+    char c = '\n'; /*Sending newline after every input*/
+    char *p_buf; /*In order to set a proper pointer address instead of memory*/
+    int ret_val, len;  /*Retrun Values and length*/
+
+    while(!feof(stdin)) 
+    {      
+        scanf("%s", buf);
+
+        len = strlen(buf);
+        p_buf = buf;
+
+        while (len > 0)
+        {
+            ret_val = send(sock_desc, p_buf, len, 0); /*Sending Byte by Byte*/     
+            if (ret_val == -1)
+            {
+                printf("cannot write to server!\n");
+                break;
+            }
+
+            p_buf += ret_val;
+            len -= ret_val;
+        }
+
+        ret_val = send(sock_desc, &c, 1, 0);   /*Sending NewLine*/   
+        if (ret_val == -1)
+        {
+            printf("cannot write to server!\n");
+            break;
+        }
+
+        if (strcmp(buf, "exit") == 0) /*Exttting client*/     
+            break;  
+    }  
+
+    close(sock_desc);  
+    printf("client disconnected\n");
+
+    return 0;  
+} 
---
+++ Week 7/25404776-status-socket-server.c
@@ -0,0 +1,89 @@
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <stdio.h>  
+#include <string.h> 
+#include <stdlib.h>  
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include "segment.h"
+
+int main() 
+{  
+
+    SEG_DATA *mydata;
+
+    int sock_desc = socket(AF_INET, SOCK_STREAM, 0); /*Creating Socet connection*/
+
+    if (sock_desc == -1) /* Error Handling*/
+    {
+        printf("cannot create socket!\n");
+        return 0;
+    }
+
+    struct sockaddr_in server;  /*Initializing socket address*/
+    memset(&server, 0, sizeof(server));  /*Allocation memory*/
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;  
+    server.sin_port = htons(8080);  /*Allocating Port and Ip address*/
+
+    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0) /*Binding Socket to server*/
+    {
+        printf("cannot bind socket!\n");
+        close(sock_desc);  
+        return 1;
+    }
+
+    if (listen(sock_desc, 20) != 0) /*Error Handling on socket*/
+    {
+        printf("cannot listen on socket!\n");
+        close(sock_desc);  
+        return 1;
+    }
+
+    struct sockaddr_in client;  /*Creating Client Input*/
+    memset(&client, 0, sizeof(client));  
+    socklen_t len = sizeof(client); 
+    int temp_sock_desc = accept(sock_desc, (struct sockaddr*)&client, &len);
+
+    if (temp_sock_desc == -1)
+    {
+        printf("Cannot accept client!\n");
+        close(sock_desc);  
+        return 1;
+    }
+
+    char buf[100];  
+    int ret_val;  
+
+    while(1) 
+    {      
+        ret_val = recv(temp_sock_desc, buf, 100, 0);   /*Getting data*/    
+        if (ret_val == -1)
+        {
+            printf("Cannot read from client!\n");
+            break;
+        }
+
+        if (ret_val == 0)
+        {
+            printf("Client disconnected.\n");
+            break;
+        }
+
+        if (ret_val > 0)          
+            printf("%*.*s", ret_val, ret_val, buf);  
+
+        if (strcmp(buf, "exit") == 0)   
+        {
+            printf("Client Closing Server.\n");      
+            break;     
+        } 
+    }
+
+    close(temp_sock_desc);  
+    close(sock_desc);  
+
+    printf("server disconnected\n");
+    return 0;  
+} 
